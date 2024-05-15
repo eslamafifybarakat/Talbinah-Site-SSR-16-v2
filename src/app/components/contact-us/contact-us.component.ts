38190692ee4abc98ec1cf41c1aac6edd11c51371
eslamfront55 <eslamfront55@gmail.com>
2024-05-15 11:41:30 +0200

    contact us

--- src/app/components/contact-us/contact-us.component.ts
+++ src/app/components/contact-us/contact-us.component.ts
@@ -1,7 +1,9 @@
+import { HomeService } from './../../services/home.service';
+import { AlertsService } from './../../services/generic/alerts.service';
 // Modules
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, isPlatformBrowser, isPlatformServer } from '@angular/common';
 
 //Services
 import { LocalizationLanguageService } from './../../services/generic/localization-language.service';
@@ -13,7 +15,7 @@ import { catchError, finalize, tap } from 'rxjs/operators';
 import { Subscription } from 'rxjs';
 
 // Components
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, Inject, PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-contact-us',
@@ -28,102 +30,112 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-us.component.scss']
 })
 export class ContactUsComponent {
-  private subscriptions: Subscription[] = [];
-  private imageBaseUrl: string = '';
-  private metaDetails: MetaDetails | undefined;
-  private fullPageUrl: string = '';
+  private unsubscribe: Subscription[] = [];
 
-
-  /* --- Start Contact Us Variables --- */
-  contactForm = this.fb.group({
-    radioGroup: this.fb.group({
-      radio: ['', Validators.required]
-    }),
-    fullName: ['', { validators: [Validators.required, Validators.minLength(3)], updateOn: 'blur' }],
-    email: ['', { validators: [Validators.required, Validators.pattern(patterns.email)], updateOn: 'blur' }],
-    phoneNumber: ['', { validators: [Validators.required], updateOn: 'blur' }],
-    message: ['', { validators: [Validators.required, Validators.minLength(3)], updateOn: 'blur' }],
-    file: ['', Validators.required]  // Add file control
-  });
+  contactForm = this.fb.group(
+    {
+      name: ['', { validators: [Validators.required, Validators.minLength(3)], updateOn: "blur" }],
+      email: ['', { validators: [Validators.required, Validators.pattern(patterns?.email)], updateOn: "blur" }],
+      subject: ['', { validators: [Validators.required, Validators.minLength(3)], updateOn: "blur" }],
+    },
+  );
   get formControls(): any {
     return this.contactForm?.controls;
   }
-  /* --- End Contact Us Variables --- */
-
-  fileName: string;
-
-  onFileSelected(event: Event): void {
-    const input = event.target as HTMLInputElement;
-    if (input.files && input.files.length) {
-      this.fileName = input.files[0].name;
-      // this.contactForm.patchValue({ file: input.files[0] });  // Set file control value
-    }
-  }
-  
-  clearFile(): void {
-    this.contactForm.patchValue({ file: null });  // Clear file control value
-    this.fileName = '';  // Clear file name display
-  }
+  isLoadingBtn: boolean = false;
 
   constructor(
-    private localizationLanguageService: LocalizationLanguageService,
+    @Inject(PLATFORM_ID) private platformId: Object,
     private metadataService: MetadataService,
+    private alertsService: AlertsService,
     public publicService: PublicService,
+    private homeService: HomeService,
+    private cdr: ChangeDetectorRef,
     private fb: FormBuilder
-  ) {
-    localizationLanguageService.updatePathAccordingLang();
-  }
+  ) { }
 
   ngOnInit(): void {
-    this.initPageData();
-  }
-  private initPageData(): void {
-    this.updateMetaTagsForSEO();
-    this.imageBaseUrl = environment.imageBaseUrl;
-  }
-  private updateMetaTagsForSEO(): void {
-    let metaData: MetaDetails = {
-      title: 'تواصل معنا',
-      description: 'الوصف',
-      image: 'https://avatars.githubusercontent.com/u/52158422?s=48&v=4'
+    if (isPlatformBrowser(this.platformId)) {
+      this.updateMetaTags();
     }
-    this.metadataService.updateMetaTagsForSEO(metaData);
+    if (isPlatformServer(this.platformId)) {
+      this.updateMetaTags();
+    }
+  }
+  private updateMetaTags(): void {
+    this.metadataService.updateTitle('تلبينة | تواصل معنا');
+    this.metadataService.updateMetaTagsName([
+      { name: 'title', content: 'تلبينة | تواصل معنا' },
+      { name: 'description', content: 'احصل على الصحة النفسية التي تحتاجها مع تطبيق تلبينة. جلسات علاج نفسي مع أفضل أخصائي الطب النفسي في السعودية، متاحة لك في راحة بمنزلك عبر الإنترنت. ابدأ علاجك النفسي اليوم' },
+      { name: 'date', content: '2023-10-29T09:28:59+00:00' },
+
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:url', content: 'http://talbinah.net/' },
+      { name: 'twitter:site', content: '@Talbinahco' },
+      { name: 'twitter:title', content: 'تلبينة | تواصل معنا' },
+      { name: 'twitter:description', content: 'احصل على الصحة النفسية التي تحتاجها مع تطبيق تلبينة. جلسات علاج نفسي مع أفضل أخصائي الطب النفسي في السعودية، متاحة لك في راحة بمنزلك عبر الإنترنت. ابدأ علاجك النفسي اليوم' },
+      { name: 'twitter:image', content: 'https://talbinah.net/assets/images/main/logos/logo_talbinah.png' },
+    ]);
+    this.metadataService.updateMetaTagsProperty([
+      { property: 'og:locale', content: 'ar_AR' },
+      { property: 'article:publisher', content: 'https://www.facebook.com/Talbinahco/' },
+      { property: 'article:modified_time', content: '2023-10-29T09:28:59+00:00' },
+
+      { property: 'og:type', content: 'website' },
+      { property: 'og:url', content: 'http://talbinah.net/' },
+      { property: 'og:title', content: 'تلبينة | تواصل معنا' },
+      { property: 'og:description', content: 'احصل على الصحة النفسية التي تحتاجها مع تطبيق تلبينة. جلسات علاج نفسي مع أفضل أخصائي الطب النفسي في السعودية، متاحة لك في راحة بمنزلك عبر الإنترنت. ابدأ علاجك النفسي اليوم' },
+      { property: 'og:image', content: 'https://talbinah.net/assets/images/main/logos/logo_talbinah.png' },
+      { property: 'twitter:site_name', content: 'تطبيق تلبينة' }
+    ]);
   }
 
-  /* --- Start Contact Us Functions --- */
-  submitContact(): void {
-    if (this.contactForm?.valid) {
-      let data = {
-        email: this.contactForm?.value?.email,
-        // password: this.contactForm?.value?.password,
-      };
-      //Send Request to login
-      // let loginSubscription: any = this.authService?.login(data)?.pipe(
-      //   tap(res => this.handleSuccessLoggedIn(res)),
-      //   catchError(err => this.handleError(err))
-      // ).subscribe();
-      // this.subscriptions.push(loginSubscription);
+  submit(): void {
+    const isFormValid = this.contactForm?.valid;
+    if (isFormValid) {
+      this.isLoadingBtn = true;
+      const formData: FormData = this.createFormData();
+      if (isPlatformBrowser(this.platformId)) {
+        this.sendContactFormData(formData);
+      }
     } else {
       this.publicService.validateAllFormFields(this.contactForm);
     }
   }
-  /* --- End Contact Us Functions --- */
-
-  /* --- Handle api requests error messages --- */
-  private handleError(err: any): any {
-    this.setErrorMessage(err || 'An error has occurred');
+  private createFormData(): FormData {
+    const formData: FormData = new FormData();
+    formData.append('full_name', this.contactForm?.value?.name);
+    formData.append('email', this.contactForm?.value?.email);
+    formData.append('message', this.contactForm?.value?.subject);
+    return formData;
   }
-  private setErrorMessage(message: string): void {
-    // Implementation for displaying the error message, e.g., using a sweetalert
-    console.log(message);
-    // this.alertsService?.openSweetAlert('error', message);
+  private sendContactFormData(formData: FormData): void {
+    this.homeService?.contactUs(formData)?.subscribe(
+      (res: any) => this.handleSuccessResponse(res),
+      (err: any) => this.handleErrorResponse(err)
+    );
+  }
+  private handleSuccessResponse(res: any): void {
+    if (res?.status == true) {
+      this.handleSuccessScenario();
+    } else {
+      const errorMessage = res?.message || 'An error occurred while sending the request.';
+      this.alertsService?.openToast('error', 'error', errorMessage);
+    }
+  }
+  private handleSuccessScenario(): void {
+    this.isLoadingBtn = false;
+    this.contactForm.reset();
+    this.alertsService?.openToast('success', 'success', 'تم إرسال طلبك بنجاح');
+    this.cdr.detectChanges();
+  }
+  private handleErrorResponse(err: any): void {
+    const errorMessage = err?.message || 'An error occurred while sending the request.';
+    this.alertsService?.openToast('error', 'error', errorMessage);
+    this.isLoadingBtn = false;
   }
 
   ngOnDestroy(): void {
-    this.subscriptions.forEach((subscription: Subscription) => {
-      if (subscription && subscription.closed) {
-        subscription.unsubscribe();
-      }
-    });
+    this.unsubscribe?.forEach((sb) => sb?.unsubscribe());
   }
 }
