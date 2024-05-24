98653a10c1e7e499cc87c25f4df3a10496e5ce81
eslamfront55 <eslamfront55@gmail.com>
2024-05-24 05:31:06 +0300

    Remove Unused Lines At Environments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,17 +2,6 @@ export const environment = {
   production: false,
   // apiUrl: "https://api.dev.talbinah.net",
   apiUrl: "https://api.talbinah.net",
-  publicUrl: "http://Talbinah.net",
-  imageBaseUrl: "https://Talbinah.net",
-  chatGptApi: "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",
-
-  firebaseConfig: {
-    apiKey: "Talbinah-YRuVXWLlZt1FAiihDTVW6K0gtDI",
-    authDomain: "Talbinah-fabeb.firebaseapp.com",
-    projectId: "Talbinah-fabeb",
-    storageBucket: "Talbinah-fabeb.appspot.com",
-    messagingSenderId: "227904650466",
-    appId: "1:227904650466:web:3be3d5c858023ff69172bb",
-    measurementId: "G-N5BX5TL73F"
-  }
+  publicUrl: "http://talbinah.net",
+  imageBaseUrl: "https://talbinah.net"
 };
