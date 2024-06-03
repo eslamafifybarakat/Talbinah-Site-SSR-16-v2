21e3c51528cff787df1b895d956741a90231e4f4
eslamfront55 <eslamfront55@gmail.com>
2024-06-03 19:10:31 +0300

    Navbar Mobile Items

--- src/app/shared/components/navbar-mobile/navbar-mobile.component.ts
+++ src/app/shared/components/navbar-mobile/navbar-mobile.component.ts
@@ -2,7 +2,7 @@
 import { CommonModule, NgOptimizedImage, isPlatformBrowser } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { SidebarModule } from 'primeng/sidebar';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router, RouterModule } from '@angular/router';
 
 //Services
 import { NavItem, navItems } from './../../../interfaces/navbar';
@@ -11,6 +11,7 @@ import { keys } from '../../configs/localstorage-key';
 
 //Components
 import { LanguageSelectorComponent } from '../language-selector/language-selector.component';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'navbar-mobile',
@@ -34,13 +35,26 @@ export class NavbarMobileComponent {
   currentLanguage: string | null = '';
 
   navItems: NavItem[];
+  currentUrl: string = '';
 
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private activatedRoute:ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     if (isPlatformBrowser(this.platformId)) {
       this.currentLanguage = localStorage.getItem(keys.language);
     }
+    // Listen to changes in the route
+    this.router?.events?.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => this.activatedRoute)
+    ).subscribe((event) => {
+      // Logic to execute on NavigationEnd
+      this.currentUrl = event?.root?.firstChild?.snapshot?.url?.join('');
+    });
     this.loadData();
   }
   loadData(): void {
